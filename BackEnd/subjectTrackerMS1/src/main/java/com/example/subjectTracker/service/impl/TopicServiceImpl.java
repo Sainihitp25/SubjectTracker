903a140ca4a09ac4afe04caa6aef6fa1d2e1d185
Mekala Narendra <narendramekala58@gmail.com>
2024-07-13 15:55:08 +0530

    update the Backend Code

---
+++ BackEnd/subjectTrackerMS1/src/main/java/com/example/subjectTracker/service/impl/TopicServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.subjectTracker.service.impl;
+
+import com.example.subjectTracker.entity.SubjectTracker;
+import com.example.subjectTracker.entity.Topic;
+import com.example.subjectTracker.exception.ResourceNotFoundException;
+import com.example.subjectTracker.repository.SubjectRepository;
+import com.example.subjectTracker.repository.TopicRepository;
+import com.example.subjectTracker.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicServiceImpl implements TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private SubjectRepository  subjectRepository;
+
+
+    @Override
+    public Topic saveTopic(Topic topic , Integer subjectId) {
+        if (subjectRepository.existsById(subjectId))
+        {
+            SubjectTracker subjectTracker = subjectRepository.findById(subjectId).orElseThrow(
+                    () -> new ResourceNotFoundException("nothing found for id"));
+            topic.setSubjectTracker(subjectTracker);
+            return topicRepository.save(topic);
+        }
+        else
+        {
+            throw new ResourceNotFoundException("nothing found for id");
+        }
+    }
+    public List<Topic> getAllTopicsBysubjectId(Integer subjectId) {
+
+        if (subjectRepository.existsById(subjectId))
+        {
+//            return topicRepository.findBysubjectId(subjectId);
+            return topicRepository.findAll();
+        }
+        else
+        {
+            throw new ResourceNotFoundException("nothing found for id");
+        }
+
+    }
+
+    public Topic getTopicById(Integer topicId) {
+        return topicRepository.findById(topicId).orElse(null);
+    }
+}
